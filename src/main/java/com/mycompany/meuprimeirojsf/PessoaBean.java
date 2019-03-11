198ff4721341266d29f3a74f6e0a42a391a14c33
caiquevitoriano <vitorianocaique@gmail.com>
2019-03-10 21:06:51 -0300

    não sei pq essas anotações não funcionam aaaa

--- src/main/java/com/mycompany/meuprimeirojsf/PessoaBean.java
+++ src/main/java/com/mycompany/meuprimeirojsf/PessoaBean.java
@@ -5,34 +5,24 @@
  */
 package com.mycompany.meuprimeirojsf;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 
-/**
- *
- * @author caique
- */
-@ManagedBean( name = "pessoaBean")
+
+
+@ViewScoped
+@ManagedBean(name = "pessoaBean")
 public class PessoaBean {
-    
+
     private String nome;
-    private String sobrenome;
-    private String nomeCompleto;
-    
-    public String montarNome(){
-        nomeCompleto = nome+" "+sobrenome;
+    private List<String> nomes = new ArrayList<String>();
+
+    public String addNome() {
+        nomes.add(nome);
         return "";
     }
-
-    public String getNomeCompleto() {
-        return nomeCompleto;
-    }
-
-    public void setNomeCompleto(String nomeCompleto) {
-        this.nomeCompleto = nomeCompleto;
-    }
-    
-    
-           
 
     public String getNome() {
         return nome;
@@ -42,14 +32,12 @@ public class PessoaBean {
         this.nome = nome;
     }
 
-    public String getSobrenome() {
-        return sobrenome;
+    public List<String> getNomes() {
+        return nomes;
     }
 
-    public void setSobrenome(String sobrenome) {
-        this.sobrenome = sobrenome;
+    public void setNomes(List<String> nomes) {
+        this.nomes = nomes;
     }
-    
-    
-    
+
 }
